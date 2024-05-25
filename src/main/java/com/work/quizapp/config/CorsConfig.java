e50d7076a66fa29e20525886193dca434aec1f9f
dolphinium <dolphofficial@gmail.com>
2024-05-25 16:41:21 +0300

    v1

--- src/main/java/com/work/quizapp/config/CorsConfig.java
+++ src/main/java/com/work/quizapp/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // Allow requests from localhost:3000
+                .allowedOrigins("http://localhost:3000","http://localhost:53310/") // Allow requests from localhost:3000
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
