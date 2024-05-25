30f60fb6f74aa431688dc5f068b90846b218fb26
dolphinium <dolphofficial@gmail.com>
2024-05-25 18:39:11 +0300

    disable CorsConfig

--- src/main/java/com/work/quizapp/config/CorsConfig.java
+++ src/main/java/com/work/quizapp/config/CorsConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+
+//@Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000","http://localhost:53310/") // Allow requests from localhost:3000
+                .allowedOrigins("*") // Allow all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
